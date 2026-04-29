5fed7cfa221164f902bffd74be2c2a00d93fcb79
nicolaslopes-lab21 <nicolas.lopes@ba.estudante.senai.br>
2026-04-29 20:44:01 -0300

    Add files via upload

---
+++ api/src/main/java/com/example/api/dto/FuncionarioResponse.java
@@ -0,0 +1,7 @@
+package com.example.api.dto;
+
+public class FuncionarioResponse {
+    private String nome;
+    private String email;
+    private String telefone;
+}
---
+++ api/src/main/java/com/example/api/repository/FuncionarioRepository.java
@@ -0,0 +1,12 @@
+package com.example.api.repository;
+
+import com.example.api.model.FuncionarioModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface FuncionarioRepository extends JpaRepository<FuncionarioModel, Long> {
+    Optional<FuncionarioModel> findByEmail(String email);
+}
